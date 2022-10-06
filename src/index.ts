ab0e0f21e0b9d86d2cb0f24f9678b013a104c4d9
Ruslan Andreev <rus.andreev@gmail.com>
2022-10-06 20:10:45 +0300

    wip

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Bob", age: 30 });
 
+user.set({ name: "Alice", age: 20 });
+
 console.log(user.get("name"));
 console.log(user.get("age"));
